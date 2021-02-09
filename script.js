37fd817a79eeb241f1cb9c0ada1172e98fba2791
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-02-08 23:11:57 -0600

    created length array in popout form

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-var 
-// have code prompt user for password length
+// Create an array for the possible lengths
+var userLengthArray = [];
+var startLeng = 8;
+
+// Adding elements to the array
+for(i = 0; i < 121; i++)
+{
+    userLengthArray.push(startLeng);
+    console.log(startLeng);
+    startLeng++
+    
+}
+
 //Save that var userLength
+var input1 = document.getElementById("input1").value.toUpperCase();
+  var item = userLengthArray.find((item) => item[input1])
+
+if(item)
+{
+    console.log(item);
+}
+
+
+
+
+
+
 //User case switch to branch off from there
 // have code prompt user for 
